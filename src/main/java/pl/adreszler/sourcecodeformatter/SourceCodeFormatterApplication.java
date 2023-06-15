27eaafbdf8c9a2ba1ccec2960a5911b5ef5cec0c
Aleksander Dreszler <olek.d93@gmail.com>
2023-06-15 13:49:50 +0200

    Project creation

---
+++ src/main/java/pl/adreszler/sourcecodeformatter/SourceCodeFormatterApplication.java
@@ -0,0 +1,13 @@
+package pl.adreszler.sourcecodeformatter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SourceCodeFormatterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SourceCodeFormatterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/adreszler/sourcecodeformatter/SourceCodeFormatterApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.adreszler.sourcecodeformatter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SourceCodeFormatterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
